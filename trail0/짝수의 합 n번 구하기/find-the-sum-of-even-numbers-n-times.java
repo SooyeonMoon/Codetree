b199ba519ce75583854b8c6e2469a72e611e84a2
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-06 23:22:20 +0900

    [Passed] 짝수의 합 n번 구하기 (141ms, 10MB)

---
+++ trail0/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int total = 0;
+            for (int p = a; p <= b; p++) {
+                if (p % 2 == 0) {
+                    total += p;
+                }
+            }
+            System.out.println(total);
+        }
+    }
+}
