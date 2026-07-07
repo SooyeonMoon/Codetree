ed478091948b2370701f851665a9bb19d1bda531
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-07 13:22:51 +0900

    [Passed] 상하로 반복하며 출력 (132ms, 10MB)

---
+++ trail0/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[][] m = new int[N][N];
+
+        int cnt = 1;
+        for (int i = 0; i < N; i++) {
+            if (cnt >= 5) {
+                    for (int j = 0; j < N; j++) {
+                        m[j][i] = --cnt;
+                }
+            }
+            else {
+                for (int j = 0; j < N; j++) {
+                    m[j][i] = cnt++;
+                }
+            }
+            
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(m[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
