d912f32bf0b61faf4fbb6d9b846962f3734646f4
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-08 10:51:58 +0900

    [Passed] 반복 출력하기 (152ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        printString(N);
+    }
+
+    static void printString(int N) {
+        for (int i = 0; i < N; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+}
