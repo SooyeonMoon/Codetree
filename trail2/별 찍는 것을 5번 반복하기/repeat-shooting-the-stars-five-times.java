9a3fbea2d99456b4f2d558527cfcab92c9e8c11a
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-08 10:48:30 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (98ms, 9MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int i = 0; i < 5; i++) {
+            printStars();
+        }
+    }
+
+    static void printStars() {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
