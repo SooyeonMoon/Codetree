ba7ba7d43cd9db2d9bcad8e34e62c5c6089e077a
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-10 00:01:24 +0900

    [Passed] 함수를 이용한 소수 판별 (123ms, 10MB)

---
+++ trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int total = 0;
+        for (int i = A; i <= B; i++) {
+            if (isPrime(i)) {
+                total += i;
+            }
+        }
+        System.out.print(total);
+    }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
