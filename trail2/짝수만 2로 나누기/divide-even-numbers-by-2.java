680d3a752566ebc5a4e2e3d3129ee7f29bf2f7a8
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-11 10:43:28 +0900

    [Passed] 짝수만 2로 나누기 (146ms, 10MB)

---
+++ trail2/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        changeArray(arr);
+     }
+
+     public static void changeArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] / 2 + " ");
+            }
+            else {
+                System.out.print(arr[i] + " ");
+            }
+        }
+     }
+}
