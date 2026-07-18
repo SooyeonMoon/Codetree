5741fff7b12c4d62d5319710f794bde1a61f5aa5
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-18 13:43:45 +0900

    [Passed] 재귀함수를 이용한 3N + 1 수열 (136ms, 11MB)

---
+++ trail2/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+
+        System.out.print(F(n));
+    }
+
+    public static int F(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return F(n / 2) + 1;
+        }
+        else {
+            return F(3 * n + 1) + 1;
+        }
+    }
+}
