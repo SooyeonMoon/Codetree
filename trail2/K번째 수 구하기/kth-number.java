67b8a9ef5bc8d967dd518c8d802692786e5a9614
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-19 17:11:21 +0900

    [Passed] K번째 수 구하기 (130ms, 10MB)

---
+++ trail2/K번째 수 구하기/kth-number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[k - 1]);
+    }
+}
