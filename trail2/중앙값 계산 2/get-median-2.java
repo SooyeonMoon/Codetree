33d9b353f99a6bc39cabdb8de279617deb17bb75
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-19 18:23:09 +0900

    [Passed] 중앙값 계산 2 (127ms, 10MB)

--- trail2/중앙값 계산 2/get-median-2.java
+++ trail2/중앙값 계산 2/get-median-2.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 public class Main {
     public static int MAX_N = 100;
 
+    public static int[] arr = new int[MAX_N];
+
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arr = new int[MAX_N];
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
@@ -17,12 +18,12 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
-                System.out.print(printMiddleNum(arr, i) + " ");
+                System.out.print(printMiddleNum(i) + " ");
             }
         }
     }
 
-    public static int printMiddleNum(int[] arr, int n) {
+    public static int printMiddleNum(int n) {
         Arrays.sort(arr, 0, n + 1);
         return arr[n / 2];
     }
