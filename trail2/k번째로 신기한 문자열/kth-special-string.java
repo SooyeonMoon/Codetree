d1f58be1a90527ab21d885250d62dfea66df1371
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-20 17:10:54 +0900

    [Passed] k번째로 신기한 문자열 (145ms, 10MB)

--- trail2/k번째로 신기한 문자열/kth-special-string.java
+++ trail2/k번째로 신기한 문자열/kth-special-string.java
@@ -2,46 +2,24 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-
-    public static String[] s;
-    public static String str;
-
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        int k = sc.nextInt(); // k번째 문자열 출력
-        str = sc.next(); // 문자열 T
-
-        s = new String[n];
-        for (int i = 0; i < s.length; i++) {
-            s[i] = sc.next();
+        int k = sc.nextInt();
+        String t = sc.next(); // 문자열 T
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = sc.next();
         }
-        Arrays.sort(s);
-
-        String[] s2 = isSequence();
-        System.out.print(s2[k - 1]);
-    }
-
-    public static String[] isSequence() {
-        String[] str = new String[s.length];
+        // Please write your code here.
+        String[] w = new String[n];
         int p = 0;
-        for (int i = 0; i < s.length; i++) {
-            if (isPart(i)) {
-                str[p++] = s[i];
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].startsWith(t)) {
+                w[p++] = words[i];
             }
         }
-        return str;
-    }
-
-    public static boolean isPart(int n) {
-        String S = s[n];
-        for (int i = 0; i < str.length(); i++) {
-            if (S.charAt(i) != str.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
+        Arrays.sort(w, 0, p);
+        System.out.print(w[k - 1]);
     }
 }
