5e8ac7ab854a7997ae84309a8599e01bc3e021bc
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-21 22:06:51 +0900

    [Passed] k번째로 신기한 문자열 (149ms, 10MB)

--- trail2/k번째로 신기한 문자열/kth-special-string.java
+++ trail2/k번째로 신기한 문자열/kth-special-string.java
@@ -4,22 +4,24 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        String t = sc.next(); // 문자열 T
+        int n = sc.nextInt(); // n개의 단어
+        int k = sc.nextInt(); // k번째
+        String t = sc.next(); // 문자열
+
         String[] words = new String[n];
         for (int i = 0; i < n; i++) {
             words[i] = sc.next();
         }
         // Please write your code here.
-        String[] w = new String[n];
+        String[] str = new String[n];
         int p = 0;
-        for (int i = 0; i < words.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (words[i].startsWith(t)) {
-                w[p++] = words[i];
+                str[p++] = words[i];
             }
         }
-        Arrays.sort(w, 0, p);
-        System.out.print(w[k - 1]);
+        Arrays.sort(str, 0, p);
+
+        System.out.print(str[k - 1]);
     }
 }
