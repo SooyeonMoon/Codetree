3dca423ee1586c9b62d0fe98e3bba187bb19ace7
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-23 16:34:08 +0900

    [Passed] 십진수로 변환하기 (136ms, 10MB)

--- trail2/십진수로 변환하기/convert-to-decimal.java
+++ trail2/십진수로 변환하기/convert-to-decimal.java
@@ -3,22 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String binary = sc.next(); // 이진수가 주어짐
-        int key = Integer.parseInt(binary);
-        int temp = key;
-
-        int p = 1;
-        while (temp >= 10) {
-            p *= 10;
-            temp /= 10;
-        }
+        String binary = sc.next();
         // Please write your code here.
         int num = 0;
-
         for (int i = 0; i < binary.length(); i++) {
-            num = num * 2 + key / p;
-            key %= p;
-            p /= 10;
+            num = num * 2 + (binary.charAt(i) - '0');
         }
         System.out.print(num);
     }
