5ea84fd74b5e14f4363b16056d1f1b341835664a
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-23 20:50:15 +0900

    [Passed] 요일 맞추기 (126ms, 10MB)

--- trail2/요일 맞추기/guess-day-of-week.java
+++ trail2/요일 맞추기/guess-day-of-week.java
@@ -1,47 +1,32 @@
 import java.util.Scanner;
-
 public class Main {
 
-    public static int[] month = new int[] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    public static String[] day = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    public static String[] days = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri" ,"Sat", "Sun"};
 
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
-
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-
-        int days = getDays(m1, d1, m2, d2);
-        // System.out.println(days);
-        if (days < 0) {
-            while (days < 0) {
-                days += 7;
-            }
+        // Please write your code here.
+        int result = calculateDays(m2, d2) - calculateDays(m1, d1);
+        while (result < 0) {
+            result += 7;
         }
-        days %= 7;
-        System.out.print(day[days]);
+        result %= 7;
+        System.out.print(days[result]);
     }
 
-    public static int getDays(int m1, int d1, int m2, int d2) {
-        int totalDays1 = 0;
-        for (int i = 1; i < m1; i++) {
-            totalDays1 += month[i];
+    public static int calculateDays(int m, int d) {
+        int[] month = new int[] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int total = 0;
+        for (int i = 1; i < m; i++) {
+            total += month[i];
         }
-        totalDays1 += d1;
-        // System.out.println(totalDays1);
+        total += d;
 
-
-        int totalDays2 = 0;
-        for (int i = 1; i < m2; i++) {
-            totalDays2 += month[i];
-        }
-        totalDays2 += d2;
-        // System.out.println(totalDays2);
-
-        return totalDays2 - totalDays1;
+        return total;
     }
 }
