6be05998e98d3330f6da5383afe0c11bace63943
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-07-25 14:14:01 +0900

    [Passed] 겹치지 않는 사각형의 넓이 (246ms, 37MB)

--- trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
+++ trail2/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int N = 3;
     public static int OFFSET = 1000;
     public static int MAX_N = 2000;
 
-    public static int[] x1 = new int[MAX_N + 1];
-    public static int[] y1 = new int[MAX_N + 1];
-    public static int[] x2 = new int[MAX_N + 1];
-    public static int[] y2 = new int[MAX_N + 1];
+    public static int[] x1 = new int[N];
+    public static int[] y1 = new int[N];
+    public static int[] x2 = new int[N];
+    public static int[] y2 = new int[N];
 
     public static int[][] arr = new int[MAX_N + 1][MAX_N + 1];
 
@@ -16,7 +17,7 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < N; i++) {
             x1[i] = sc.nextInt();
             y1[i] = sc.nextInt();
             x2[i] = sc.nextInt();
@@ -28,24 +29,18 @@ public class Main {
             y2[i] += OFFSET;
         }
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < N; i++) {
             for (int a = x1[i]; a < x2[i]; a++) {
                 for (int b = y1[i]; b < y2[i]; b++) {
-                    arr[a][b] = 1;
+                    arr[a][b] = i + 1;
                 }
-            }
-        }
-
-        for (int i = x1[2]; i < x2[2]; i++) {
-            for (int j = y1[2]; j < y2[2]; j++) {
-                arr[i][j] = 0;
             }
         }
 
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (arr[i][j] == 1) {
+                if (arr[i][j] == 1 || arr[i][j] == 2) {
                     count++;
                 }
             }
