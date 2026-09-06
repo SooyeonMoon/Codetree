d3176ee14e8030984e0d740384f4d2341bdb14c6
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-09-06 13:02:47 +0900

    [Passed] 문자에 따른 명령 2 (228ms, 13MB)

--- trail2/문자에 따른 명령 2/text-based-commands2.java
+++ trail2/문자에 따른 명령 2/text-based-commands2.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int[] dx = new int[] {1, 0, -1, 0};
-    public static int[] dy = new int[] {0, -1, 0, 1};
+
+    public static int dx[] = {0, 1, 0, -1};
+    public static int dy[] = {1, 0, -1, 0};
+
+    public static int cNum = 0;
 
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // Please write your code here.
 
-        int dirNum = 3;
-        int posX = 0, posY = 0;
-        
-        String str = sc.next();
+        int x = 0, y = 0;
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'L') {
-                dirNum = (dirNum + 3) % 4; //counterclockwise
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                cNum = (cNum + 3) % 4;
             }
-            else if (str.charAt(i) == 'R') {
-                dirNum = (dirNum + 1) % 4; // clockwise
+            else if (s.charAt(i) == 'R') {
+                cNum = (cNum + 1) % 4;
             }
-            else { // str.charAt(i) == 'F'인 경우
-                posX += dx[dirNum];
-                posY += dy[dirNum];
+            else {
+                x += dx[cNum];
+                y += dy[cNum];
             }
         }
 
-        
-
-        System.out.print(posX + " " + posY);
+        System.out.print(x + " " + y);
     }
 }
