b40569d05cc055a13f15680b454c4f37d92085a3
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-09-06 12:43:36 +0900

    [Passed] 방향에 맞춰 이동 (149ms, 10MB)

--- trail2/방향에 맞춰 이동/move-in-direction.java
+++ trail2/방향에 맞춰 이동/move-in-direction.java
@@ -1,41 +1,38 @@
 import java.util.Scanner;
-
 public class Main {
-    // public static int[] dx = new int[] {1, 0, -1, 0};
-    // public static int[] dy = new int[] {0, -1, 0, 1};
+
+    public static int dx[] = {1, 0, -1, 0};
+    public static int dy[] = {0, -1, 0, 1};
+
+    public static char dir[] = {'E', 'S', 'W', 'N'};
+
+    public static int x = 0;
+    public static int y = 0;
 
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int posX = 0, posY = 0;
-
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // n번 이동
         for (int i = 0; i < n; i++) {
-            char d = sc.next().charAt(0);
-            int x = sc.nextInt();
+            char direction = sc.next().charAt(0);
+            int distance = sc.nextInt();
+            // Please write your code here.
 
-            int key = fourDirection(d);
-            
-            if (key == 0)
-                posX += x;
-            else if (key == 1)
-                posY -= x;
-            else if (key == 2)
-                posX -= x;
-            else
-                posY += x;
+            if (direction == 'E') {
+                x += distance;
+            }
+            else if (direction == 'S') {
+                y -= distance;
+            }
+            else if (direction == 'W') {
+                x -= distance;
+            }
+            else {
+                y += distance;
+            }
+
         }
-        System.out.print(posX + " " + posY);
-    }
 
-    public static int fourDirection(char d) {
-        if (d == 'E')
-            return 0;
-        else if (d == 'S')
-            return 1;
-        else if (d == 'W')
-            return 2;
-        else
-            return 3;
+        System.out.print(x + " ");
+        System.out.print(y);
     }
 }
