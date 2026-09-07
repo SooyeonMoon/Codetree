e38f73c7bd677beba5515b724057a6207efa39c4
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-09-07 13:35:08 +0900

    [Passed] 1이 3개 이상 있는 위치 (316ms, 21MB)

--- trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ trail2/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -1,47 +1,55 @@
 import java.util.Scanner;
-
 public class Main {
-    public static int n;
 
-    public static int[] dx = new int[] {0, 1, 0, -1};
-    public static int[] dy = new int[] {1, 0, -1, 0};
-    public static int[][] count;
+    public static int N;
+    
+    public static int[][] arr;
+
+    public static int dx[] = {0, 1, 0, -1};
+    public static int dy[] = {1, 0, -1, 0};
 
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
 
-        n = sc.nextInt();
-        int[][] array = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                array[i][j] = sc.nextInt();
+        N = sc.nextInt();
+        arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        // Please write your code here.
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (countNum(i, j))
+                    count++;
             }
         }
 
-        int x = 0, y = 0;
-        int cnt = 0, ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int dirNum = 0; dirNum < 4; dirNum++) {
-                    int nx = i + dx[dirNum], ny = j + dy[dirNum];
-
-                    if (isRange(nx, ny) && array[nx][ny] == 1) {
-                        cnt++;
-                    }
-                }
-                if (cnt >= 3) { // 상하좌우 1인 개수가 3 이상이면
-                    // System.out.println(i + " " + j);
-                    ans++;
-                }
-                cnt = 0; // 상하좌우 확인용 변수 초기화
-            }
-        }
-        System.out.print(ans);
+        System.out.print(count);
     }
 
-    public static boolean isRange(int x, int y) {
-        return (x >= 0 && x < n && y >= 0 && y < n);
+    public static boolean inRange(int x, int y) {
+        if (x >= 0 && x < N && y >= 0 && y < N)
+            return true;
+        return false;
+    }
+
+    public static boolean countNum(int x, int y) {
+        int count = 0, nx, ny;
+
+        for (int dirNum = 0; dirNum < 4; dirNum++) {
+            nx = x + dx[dirNum];
+            ny = y + dy[dirNum];
+            if (inRange(nx, ny) && arr[nx][ny] == 1) {
+                count++;
+            }
+        }
+
+        if (count >= 3) {
+            return true;
+        }
+        return false;
     }
 }
