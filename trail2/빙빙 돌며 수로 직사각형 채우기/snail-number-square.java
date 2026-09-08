f85d26881fcf9be899ee5cde6e67945c27917a27
SooyeonMoon <sooyeon.moon27@gmail.com>
2026-09-08 20:29:24 +0900

    [Passed] 빙빙 돌며 수로 직사각형 채우기 (152ms, 10MB)

---
+++ trail2/빙빙 돌며 수로 직사각형 채우기/snail-number-square.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int MAX_N = 100;
+    public static int MAX_M = 100;
+    public static int n;
+    public static int m;
+
+    public static int[][] answer = new int[MAX_N][MAX_M];
+
+    public static int x = 0, y = 0; // 첫 시작 위치 (0, 0)
+    public static int dirNum = 0; // 첫 이동방향 : 오른쪽
+
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {1, 0, -1, 0};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        // Please write your code here.
+        answer[x][y] = 1;
+        int nx, ny;
+
+        for (int i = 2; i <= n * m; i++) {
+            nx = x + dx[dirNum];
+            ny = y + dy[dirNum];
+
+            if (!inRange(nx, ny) || answer[nx][ny] != 0) {
+                dirNum = (dirNum + 1) % 4;
+                nx = x + dx[dirNum];
+                ny = y + dy[dirNum];
+                // System.out.println(dirNum);
+            }
+
+            answer[nx][ny] = i;
+            x = x + dx[dirNum];
+            y = y + dy[dirNum];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(answer[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean inRange(int x, int y) {
+        return (x >= 0 && x < n && y >= 0 && y < m);
+    }
+}
